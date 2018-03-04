7ba9d34cc8a8ebeb9c4e42948cc475ea68e90de1
Conan Lian <Conan.Lian@sandisk.com>
2018-03-05 01:54:27 +0800

    act script add user input

--- dic_ac_timing.py
+++ dic_ac_timing.py
@@ -1,6 +1,20 @@
-file_address = 'file_path'
-f = open(file_address, 'r')
+import os
 
+# file_name = 'C:/workspace/job/data/4DIC board Correlation/LB01/tb_ac_timing_LB01_LT.txt'
+file_name = input("Please input the file you want to process: \n")
+# if the file user input is not exist, input again
+while True:
+    if os.path.exists(file_name):
+        break
+    else:
+        file_name = input("Can't find this file, Please input the right path and file name: \n")
+
+file_name_path = os.path.dirname(file_name)
+
+# the result file, the path is user-defined
+file_result = file_name_path + '/result.txt'
+
+f = open(file_name, 'r')
 # timing and data are deposited in these two lists, respectively
 list_data = []
 list_timing = []
@@ -32,34 +46,21 @@ j = 0
 while j < len(list_timing):
     list_timing[j] = list_timing[j].lstrip('CHAR_LOOP')
     j += 1
-
+f.close()
 # list_timing , data_temp
-k = 0
-for timing in list_timing:
-    # print(timing.endswith('\n'))
-    # there must be an assignment, otherwise, the string won't change
-    timing = timing.strip()
-    print(timing, end=' ')
+with open(file_result, 'w') as result_f:
+    k = 0
+    for timing in list_timing:
+        # print(timing.endswith('\n'))
+        # there must be an assignment, otherwise, the string won't change
+        timing = timing.strip().split('ac_')[1]
+        result_f.write('{0:25}'.format(timing) + '\t')
 
-    while k < len(data_temp):
-        if (k + 1) < len(data_temp) and 'RPT' in data_temp[k + 1]:
-            k += 1
-        else:
-            print(data_temp[k])
-            k += 1
-            break
-
-    # while k < len(data_temp):
-    #     if 'RPT' not in data_temp[k]:
-    #         print(data_temp[k])
-    #         if (k + 1) < len(data_temp) and 'RPT' not in data_temp[k + 1]:
-    #             k += 1
-    #             break
-    #
-    #     elif 'RPT' in data_temp[k]:
-    #         print(timing, end=' ')
-    #         print(data_temp[k])
-    #         if (k + 1) < len(data_temp) and 'RPT' not in data_temp[k + 1]:
-    #             k += 1
-    #             break
-    #     k += 1
+        while k < len(data_temp):
+            if (k + 1) < len(data_temp) and 'RPT' in data_temp[k + 1]:
+                k += 1
+            else:
+                result_f.write('{:6}'.format(data_temp[k].lstrip()))
+                k += 1
+                break
+result_f.close()
