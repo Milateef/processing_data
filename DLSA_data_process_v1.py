37a763bd46a1d3054c5e12ea92f1d8126f045fef
Conan Lian <Conan.Lian@sandisk.com>
2018-03-22 16:38:24 +0800

    all DUT file is ok except high light default dac value

--- DLSA_data_process_v1.py
+++ DLSA_data_process_v1.py
@@ -5,9 +5,12 @@ import os
 current_dir = os.getcwd()
 file_list = os.listdir(current_dir)
 
-SDE_margin = {}
-VDD_margin = {}
-VDDSA_margin = {}
+SDE_header = []
+VDD_header = []
+VDDSA_header = []
+VDD_flag = 0
+VDDSA_flag = 0
+VCC_flag = 0
 
 SDE_DAC = []
 VDD_DAC = []
@@ -79,12 +82,6 @@ for source_file_name in file_list:
             if get_index(source_file_name, 'DUT') != -1:
                 end_index = get_index(source_file_name, 'DUT')
                 sheet_name = source_file_name[start_index: end_index + 4]
-            elif get_index(source_file_name, 'VDDSA') != -1:
-                end_index = get_index(source_file_name, 'VDDSA')
-                sheet_name = source_file_name[start_index: end_index + 5]
-            elif get_index(source_file_name, 'VDD') != -1:
-                end_index = get_index(source_file_name, 'VDD')
-                sheet_name = source_file_name[start_index: end_index + 3]
 
             ws = wb.create_sheet(sheet_name)
 
@@ -128,6 +125,17 @@ for source_file_name in file_list:
                 """
                 column_num = 1
                 for cell_value in line_split_list:
+                    if 'SDE' in cell_value and cell_value not in SDE_header:
+                        SDE_header.append(cell_value)
+                    if 'VDDSA' in cell_value:
+                        VDDSA_flag = 1
+                    elif 'VDD' in cell_value:
+                        VDD_flag = 1
+
+                    if VDDSA_flag == 1 and cell_value not in VDDSA_header:
+                        VDDSA_header.append(cell_value)
+                    elif VDD_flag == 1 and cell_value not in VDD_header:
+                        VDD_header.append(cell_value)
 
                     cell_num_value = str_to_num(cell_value)
                     if cell_num_value != -1:
@@ -158,10 +166,73 @@ for source_file_name in file_list:
                     column_num = column_num + 1
 
                 row_num = row_num + 1
+                VDD_flag = 0
+                VDDSA_flag = 0
 
                 # if 'SDE' in line_of_sf:
                 #     shmoo_point_list = line_of_sf.rstrip().split(',')
         sf.close()
+
+for source_file_name in file_list:
+    if 'tb_DLSA_Test_SDE' in source_file_name and 'DUT' not in source_file_name:
+        with open(source_file_name, 'r') as sf:
+            start_index = source_file_name.index('SDE')
+
+            if get_index(source_file_name, 'VDDSA') != -1:
+                end_index = get_index(source_file_name, 'VDDSA')
+                sheet_name = source_file_name[start_index: end_index + 5]
+            elif get_index(source_file_name, 'VDD') != -1:
+                end_index = get_index(source_file_name, 'VDD')
+                sheet_name = source_file_name[start_index: end_index + 3]
+
+            ws = wb.create_sheet(sheet_name)
+
+            row_num = 1
+            for line_of_sf in sf:
+                line_split_list = line_of_sf.rstrip().split(',')
+                column_num = 1
+                for cell_value in line_split_list:
+                    cell_num_value = str_to_num(cell_value)
+                    if cell_num_value != -1:
+                        ws.cell(row=row_num,
+                                column=column_num).value = cell_value
+                        if cell_num_value != 0:
+                            ws.cell(row=row_num,
+                                    column=column_num).fill = redFill
+                        else:
+                            ws.cell(row=row_num,
+                                    column=column_num).fill = greenFill
+                    else:
+                        ws.cell(row=row_num, column=column_num,
+                                value=cell_value)
+                    column_num = column_num + 1
+                row_num = row_num + 1
+            end_row = row_num
+
+            ws.insert_cols(1, amount=1)
+            for row_num in range(1, end_row):
+                if 'Vcc' in ws.cell(row=row_num, column=2).value:
+                    VCC_flag = 1
+                cell_num_value = str_to_num(
+                    ws.cell(row=row_num, column=2).value)
+                if cell_num_value != -1 and VCC_flag == 1:
+                    ws.insert_rows(row_num, amount=1)
+                    VCC_flag = 0
+
+                    for index in range(0, len(SDE_header)):
+                        ws.cell(row=row_num+1+index,
+                                column=1).value = SDE_header[index]
+                    if 'VDDSA' in source_file_name:
+                        ws.cell(row=row_num, column=1).value = 'VDDSA='
+                        for index in range(0, len(VDDSA_header)-1):
+                            ws.cell(row=row_num, column=index +
+                                    2).value = VDDSA_header[index+1]
+                    elif 'VDD' in source_file_name:
+                        ws.cell(row=row_num, column=1).value = 'VDD='
+                        for index in range(0, len(VDD_header)-1):
+                            ws.cell(row=row_num, column=index +
+                                    2).value = VDD_header[index+1]
+
 
 del_sheet = wb.get_sheet_by_name('Sheet')
 wb.remove_sheet(del_sheet)
